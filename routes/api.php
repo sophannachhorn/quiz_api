cb855271aba2e85254d0fa4912bd446e5cc8c3bd
sophannachhorn <chhonsophana017@gmail.com>
2021-11-05 11:50:49 +0700

    quiz_api

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,9 @@ Route::post('authors', [AuthorController::class,'store']);
 Route::get('authors/{id}', [AuthorController::class,'show']);
 Route::put('authors/{id}', [AuthorController::class,'update']);
 Route::get('authors/{id}', [AuthorController::class,'destory']);
+
+Route::get('books', [AuthorController::class,'index']);
+Route::post('books', [AuthorController::class,'store']);
+Route::get('books/{id}', [AuthorController::class,'show']);
+Route::put('books/{id}', [AuthorController::class,'update']);
+Route::get('books/{id}', [AuthorController::class,'destory']);
